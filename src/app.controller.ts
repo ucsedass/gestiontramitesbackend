20fb06b89107ea8cf1505125e36715ff2d6bb5fb
Rodrigo Joaquin Guzman <joaquin.guzman@ucse.net.ar>
2024-08-05 20:00:58 -0300

    con las primeras apis

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,14 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Post('/traerclasestramites')
+  traerclasestramites(@Body() body: any) {
+    return this.appService.traerClasesTramites(body);
+  }
+
+  @Post('/traertipotramites')
+  traerTipoTramite(@Body() body: any) {
+    return this.appService.traerTipoTramites(body);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,46 @@
 import { Injectable } from '@nestjs/common';
 
+require('tls').DEFAULT_MIN_VERSION = 'TLSv1';
+var sql = require('mssql');
+
+var config = {
+  user: 'GestionTramites',
+  password: 'GT20240708',
+  server: 'sgo-desarrollo',
+  database: 'GestionTramites',
+
+  options: {
+    trustedConnection: true,
+    encrypt: false, // modifique a false para que no me de error de certificado
+    enableArithAbort: true,
+    trustServerCertificate: false,
+  },
+};
+
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'desde el servicio de gestion de tramites';
+  }
+
+  async traerClasesTramites(body: any) {
+    try {
+      let consulta = 'select * from claseTramites';
+      await sql.connect(config);
+      const result = await sql.query(consulta);
+      return result.recordsets[0];
+    } catch (err) {
+      return err;
+    }
+  }
+  async traerTipoTramites(body: any) {
+    try {
+      let consulta = 'select * from tipoTramites';
+      await sql.connect(config);
+      const result = await sql.query(consulta);
+      return result.recordsets[0];
+    } catch (err) {
+      return err;
+    }
   }
 }
